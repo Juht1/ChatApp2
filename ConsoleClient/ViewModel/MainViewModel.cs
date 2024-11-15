7f61f28aeab80aff1c812b1ec4862d55bbfe2089
Juht1 <130434230+Juht1@users.noreply.github.com>
2024-11-15 11:55:36 +0200

    Console

---
+++ ConsoleClient/ViewModel/MainViewModel.cs
@@ -0,0 +1,72 @@
+﻿using ChatClient.Net;
+using ConsoleClient.MVVM.Model;
+using ConsoleClient.Net;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ConsoleClient.ViewModel
+{
+    class MainViewModel
+    {
+        public ObservableCollection<UserModel> Users { get; set; }
+        public ObservableCollection<string> Messages { get; set; }
+
+
+        public string Username {  get; set; }
+        public string Message { get; set; }
+
+        private Server _server;
+
+        public MainViewModel() 
+        {
+            Users = new ObservableCollection<UserModel>();
+            Messages = new ObservableCollection<string>();
+
+            _server = new Server();
+            _server.connectedEvent += UserConnected;
+            _server.msgRecievedEvent += MessageRecieved;
+            _server.userDisonnectEvent += RemoveUser;
+            //ConnectToServerCommand = new RelayCommand(o => _server.ConnectToServer(Username), o => !string.IsNullOrEmpty(Username));
+
+            //SendMessageCommand = new RelayCommand(o => _server.SendMessageToServer(Message), o => !string.IsNullOrEmpty(Message));
+        }
+
+        private void RemoveUser()
+        {
+            var uid = _server.PacketReader.ReadMessage();
+            var user = Users.Where(x => x.UID == uid).FirstOrDefault();
+
+            Console.WriteLine($"{user} has been removed");
+            //Application.Current.Dispatcher.Invoke(() => Users.Remove(user));
+        }
+
+        private void MessageRecieved()
+        {
+            var msg = _server.PacketReader.ReadMessage();
+            Console.WriteLine(msg);
+
+            //Application.Current.Dispatcher.Invoke(() => Messages.Add(msg));
+        }
+
+        private void UserConnected()
+        {
+            var user = new UserModel
+            {
+                UserName = _server.PacketReader.ReadMessage(),
+                UID = _server.PacketReader.ReadMessage(),
+            };
+
+            if (!Users.Any(x => x.UID == user.UID))
+            {
+                //Application.Current.Dispatcher.Invoke(() => Users.Add(user));
+            }
+        }
+
+
+    }
+}
