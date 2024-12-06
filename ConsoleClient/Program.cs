8911186c77182df65f8d4d7d80ef17fd6b720aa1
Opilane TTHK <opilane@tthk.ee>
2024-12-06 11:02:14 +0200

    put into the full cde

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ConsoleClient.ViewModel;
+using System;
 
 namespace ConsoleClient
 {
@@ -8,11 +9,21 @@ namespace ConsoleClient
         {
             Console.Write("Enter your username: ");
             var name = Console.ReadLine();
+
             MainViewModel mainViewModel = new MainViewModel(name);
+
+            Console.WriteLine("You can start sending messages. Type 'exit' to quit.");
 
             while (true)
             {
+                Console.Write("Enter message: ");
                 var message = Console.ReadLine();
+
+                if (message.Equals("exit", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Exiting the chat client...");
+                    break; 
+                }
                 mainViewModel.Message = message;
                 mainViewModel.SendMessage();
             }
